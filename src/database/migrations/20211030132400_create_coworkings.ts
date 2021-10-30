62ed09d95e392d874758f4b5be73063cc4bcc105
Cesar Kohl <84682981+cesarkohl-voxie@users.noreply.github.com>
2021-10-30 11:42:26 -0300

    Update create coworkings migration.

--- src/database/migrations/20211030132400_create_coworkings.ts
+++ src/database/migrations/20211030132400_create_coworkings.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
             id int(11) NOT NULL AUTO_INCREMENT,
             name varchar(100) NOT NULL DEFAULT "",
             created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            updated_at timestamp NULL DEFAULT NULL,
+            updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
             deleted_at timestamp NULL DEFAULT NULL,
             PRIMARY KEY (id),
             UNIQUE KEY coworkings_id_uindex (id)
