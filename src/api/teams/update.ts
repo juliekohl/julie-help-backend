74592dd850a9d4265d92aa0b4df70fe01f024233
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-11-05 18:43:21 -0300

    Split all teams endpoints.

---
+++ src/api/teams/update.ts
@@ -0,0 +1,48 @@
+import teams from "./teams";
+
+const update = (teams, app, database, crypto) => {
+    /**
+     * Update
+     * POST /team/:id { coworking_id, user_id }
+     */
+    app.post('/teams/:id', async (req, res) => {
+        try {
+            const id: number = Number(req.params.id);
+
+            // Select Team
+            let team = await database
+                .select("user_id")
+                .table("teams")
+                .where({id})
+
+            const teamResult = await team;
+            const userId: number = teamResult[0].user_id;
+
+            // Update User
+            const update: { name?: string; password?: string } = {
+                name: req.body.name
+            };
+
+            if(req.body.name){
+                update.name
+            }
+
+            if (req.body.password) {
+                update.password = crypto
+                    .createHash('md5')
+                    .update(req.body.password)
+                    .digest('hex');
+            }
+
+            await database
+                .update(update)
+                .table("users")
+                .where({id: userId})
+
+            res.json({ message: 'Success' });
+        } catch (err) {
+            res.json({ message: err.sqlMessage });
+        }
+    });
+}
+export default update
