a8b650f055c9a5fc057a87067be0b4f4a3c9d981
Juliana Cochenski <juliana.cochenski@gmail.com>
2021-11-29 18:33:46 -0300

    Improvement.

--- src/api/Offices/delete.ts
+++ src/api/Offices/delete.ts
@@ -7,18 +7,8 @@ export const del = (app, db) => {
         try {
             const id: number = Number(req.params.id);
 
-            // Select office
-            let office = await db
-                .select("coworking_id")
-                .table("offices")
-                .where({id})
-
-            const officeResult = await office;
-            const coworkingId: number = officeResult[0].coworking_id;
-
             // Delete
             await db.delete().table("offices").where({id})
-            await db.delete().table("coworkings").where({id: coworkingId})
 
             res.json({ message: 'Success' });
         } catch (err) {
